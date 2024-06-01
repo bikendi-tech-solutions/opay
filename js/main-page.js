6a26abb19699f169b1ce53614a0c635c90202f65
bikendi-tech-solutions <bikenditechsolutions@gmail.com>
2024-06-02 00:41:07 +0100

    1.0.7 new

--- js/main-page.js
+++ js/main-page.js
@@ -2,6 +2,21 @@
 document.addEventListener("DOMContentLoaded",function(){
 
 
+        var opayBalHidden = Cookies.get('opayBalHidden');
+
+        if(opayBalHidden == "yes"){
+            jQuery(".toggle-bal-visible").removeClass("fa-eye");
+            jQuery(".toggle-bal-visible").addClass("fa-eye-slash");
+            jQuery(".amount-currency").hide();
+            jQuery(".opay-main-2nd .amount").text("*****");
+        }else{
+            jQuery(".toggle-bal-visible").addClass("fa-eye");
+            jQuery(".toggle-bal-visible").removeClass("fa-eye-slash");
+            jQuery(".amount-currency").show();
+            jQuery(".opay-main-2nd .amount").text(user_balance);
+        }
+        //console.log(username); 
+
     var user_balance = jQuery(".opay-main-2nd .amount").text();
 
     jQuery(".toggle-bal-visible").on("click",function(){
@@ -10,12 +25,14 @@ document.addEventListener("DOMContentLoaded",function(){
             jQuery(this).addClass("fa-eye-slash");
             jQuery(".amount-currency").hide();
             jQuery(".opay-main-2nd .amount").text("*****");
+            Cookies.set('opayBalHidden', 'yes', { expires: 365 });
            
         }else{
             jQuery(this).addClass("fa-eye");
             jQuery(this).removeClass("fa-eye-slash");
             jQuery(".amount-currency").show();
             jQuery(".opay-main-2nd .amount").text(user_balance);
+            Cookies.set('opayBalHidden', 'no', { expires: 365 });
 
         }
     });
