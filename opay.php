f01163a2ad94bf303e76f13a5354f1569aada129
bikendi-tech-solutions <bikenditechsolutions@gmail.com>
2024-06-19 07:01:46 +0100

    1.1.2

--- opay.php
+++ opay.php
@@ -10,7 +10,7 @@
 *Plugin Name: Opay Template
 *Plugin URI: http://vtupress.com
 *Description: This add Opay template into your vtu website
-*Version: 1.1.1
+*Version: 1.1.2
 *Author: Akor Victor
 *Author URI: https://facebook.com/vtupressceo
 Requires PHP: 7.4
